3203c473aa6279eee8422bafb44c955dc9f5327c
ralphbhe <71038262+ralphbhe@users.noreply.github.com>
2022-08-12 06:29:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Mobile Version
+
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+})
